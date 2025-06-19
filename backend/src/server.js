b71027931d0475a7acba0797fc6ddfced11f6303
hungbkk66 <laiviethung2003>
2025-06-20 00:20:27 +0700

    user api

--- backend/src/server.js
+++ backend/src/server.js
@@ -2,20 +2,22 @@ import express from "express";
 import "dotenv/config";
 import authRoutes from "./routes/auth.route.js";
 import userRoutes from "./routes/user.route.js";
+import chatRoutes from "./routes/chat.route.js";
 import connectDB from "./configs/db.js";
 import cookieParser from "cookie-parser";
 
-const app=express();
-const PORT=process.env.PORT || 5001;
+const app = express();
+const PORT = process.env.PORT || 5001;
 
 app.use(express.json());
-app.use(express.urlencoded({extended:true}));
+app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 
-app.use('/api/auth',authRoutes);
-app.use('/api/users',userRoutes);
+app.use('/api/auth', authRoutes);
+app.use('/api/users', userRoutes);
+app.use('/api/chat', chatRoutes)
 
-app.listen(PORT,()=>{
+app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
     connectDB();
 });
