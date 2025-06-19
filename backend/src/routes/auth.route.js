4f16475fbeec3b2ed82428fed96f57f98fb7f8df
hungbkk66 <laiviethung2003>
2025-06-19 16:49:50 +0700

    signup, login,logout api

--- backend/src/routes/auth.route.js
+++ backend/src/routes/auth.route.js
@@ -1,11 +1,17 @@
 import express from "express";
-import { signup } from "../controllers/auth.controller";
+import { signup, login, logout, onboard } from "../controllers/auth.controller.js";
+import { protectRoute } from "../middleware/auth.middleware.js";
 
 const router = express.Router();
 
-router.get('/signup',signup);
-router.get('/login',login);
-router.get('/logout',logout);
+router.post('/signup',signup);
+router.post('/login',login);
+router.post('/logout',logout);
+router.post('/onboarding',protectRoute,onboard);
 
+//check if user logged in
+router.get('/me',protectRoute,(req,res)=>{
+    res.status(200).json({success:true,user:req.user});
+})
 
 export default router;
--- backend/src/server.js
+++ backend/src/server.js
@@ -1,12 +1,22 @@
 import express from "express";
 import "dotenv/config";
+import authRoutes from "./routes/auth.route.js";
+import userRoutes from "./routes/user.route.js";
+import connectDB from "./configs/db.js";
+import cookieParser from "cookie-parser";
 
 const app=express();
 const PORT=process.env.PORT || 5001;
 
+app.use(express.json());
+app.use(express.urlencoded({extended:true}));
+app.use(cookieParser());
+
 app.use('/api/auth',authRoutes);
+app.use('/api/users',userRoutes);
 
 app.listen(PORT,()=>{
     console.log(`Server is running on port ${PORT}`);
+    connectDB();
 });
 
